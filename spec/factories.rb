78a10368835ed11a904f7f61bfc081812f7cc628
Tim Dussinger <tritowntim@gmail.com>
2012-09-13 09:32:22 -0400

    Basic working profile page with dynamic user details

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+
+FactoryGirl.define do
+	factory :user do
+		name	"Tim Dussinger"
+		email "tritowntim@gmail.com"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,4 +16,15 @@ describe "UserPages" do
 
 	end
 
+	describe "Profile page" do 
+		
+		# Code to make a user variable
+		let(:user) { FactoryGirl.create(:user) }
+		before { visit user_path(user) }
+
+		it { should have_selector('h1', :text => user.name) }
+		it { should have_selector('title', :text => user.name) }
+
+	end
+	
 end
