a8d78ef609878cac6d8321da80ea79e209d715b9
Tim Dussinger <tritowntim@gmail.com>
2012-09-16 10:22:24 -0400

    Working user editing, protected by authorization filters

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+  include ApplicationHelper  #needed to access helper method
 
 describe "Authentication" do
 
@@ -29,20 +30,71 @@ describe "Authentication" do
 
   describe "with valid information" do
   	let(:user) { FactoryGirl.create(:user) }
-  	before do
-  		fill_in "Email", :with => user.email
-			fill_in "Password", :with => user.password
-			click_button "Sign in"
-		end
+  	before { sign_in user }
 
 		it { should have_selector('title', :text => user.name) }
 		it { should have_link('Profile', :href => user_path(user)) }
+		it { should have_link('Settings', :href => edit_user_path(user)) }
 		it { should have_link('Sign out', :href => signout_path) }
 		it { should_not have_link('Sign in', :href => signin_path) }
 
 		describe "followed by signout" do
 			before { click_link "Sign out" }
-			it { should have_link('Sign in') }
+			# it { should have_link('Sign in') }
+		end
+
+	end
+
+	describe "authorization" do 
+
+		describe "for non-signed-in users" do 
+			let(:user) { FactoryGirl.create(:user) }
+
+			describe "visiting the edit page" do 
+				before { visit edit_user_path(user) }
+				it { should have_selector('title', :text => 'Sign in')}
+			end
+
+			describe "submitting to update action" do
+				# using PUT to do what browser cannot do
+				before { put user_path(user) }
+				# response here is test of server response itself
+				specify { response.should redirect_to(signin_path) }
+			end
+
+			describe "when attempting to visit protected page" do
+				before do
+					visit edit_user_path(user)
+					fill_in "Email", :with => user.email
+					fill_in "Password", :with => user.password
+					click_button "Sign in"
+				end
+
+				describe "after signing in" do
+					it "should have rendered desired protected page" do
+						page.should have_selector('title', :text => 'Edit user')
+					end
+				end
+
+			end
+
+		end
+
+		describe "as wrong user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:wrong_user) { FactoryGirl.create(:user, :email => "wrong@example.com")}
+			before { sign_in user }
+
+			describe "visiting Users#edit page"	do
+				before { visit edit_user_path(wrong_user) }
+				it { should_not have_selector('title', :text => full_title('Edit user'))}
+			end
+
+			describe "submitting PUT to Users#update action" do
+				before { put user_path(wrong_user) }
+				specify { response.should redirect_to(root_path) }
+			end
+		
 		end
 
 	end
